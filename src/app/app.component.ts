4837fa882449a853232dac813b7100ca5cc02f78
WILLIAM NICOLAS BUITRAGO CAMACHO <1233506653@bolivar.com>
2022-10-26 00:49:43 -0500

    Interpolación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  nombre:String="Nicolas Buitrago"
-  apellido:String="Camacho"
+  /**
+   * Interpolación
+   * Se define el atributo que se va a interpolar en el template
+   */
+  clienteActual: String = 'William Nicolás Buitrago Camacho';
+  urlImagen: String =
+    'https://images.pexels.com/photos/2014422/pexels-photo-2014422.jpeg?auto=compress&cs=tinysrgb&h=350';
 }
